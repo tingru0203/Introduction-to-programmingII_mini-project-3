62ed1d766b7afa36f122d2c1562bf1aacdb4d9e8
tingru <ruby20010203@gmail.com>
2020-06-28 15:15:09 +0800

    final

--- player_1.cpp
+++ player_1.cpp
@@ -60,48 +60,107 @@ public:
         value = s.value;
         game_end = s.game_end;
     }
-    int corner(int x, int y) {
-        int val = 500;
-        if(x == 0 && y == 0) {
+    int stability() {
+        const int edge = 200;
+        int val = 0;
+        if(othello[0][0]) {
+            int mul = (othello[0][0] == player)? 1 : -1;
             for(int i = 1; i < 7; i++) {
-                if(othello[x][y] == othello[i][y]) val += 50;
+                if(othello[0][0] == othello[i][0]) val += mul * edge;
                 else break;
             }
             for(int j = 1; j < 7; j++) {
-                if(othello[x][y] == othello[x][j]) val += 50;
+                if(othello[0][0] == othello[0][j]) val += mul * edge;
                 else break;
             }
         }
-        else if(x == 0 && y == 7) {
+        if(othello[0][7]) {
+            int mul = (othello[0][7] == player)? 1 : -1;
             for(int i = 1; i < 7; i++) {
-                if(othello[x][y] == othello[i][y]) val += 50;
+                if(othello[0][7] == othello[i][7]) val += mul * edge;
                 else break;
             }
             for(int j = 6; j > 0; j--) {
-                if(othello[x][y] == othello[x][j]) val += 50;
+                if(othello[0][7] == othello[0][j]) val += mul * edge;
                 else break;
             }
         }
-        else if(x == 7 && y == 0) {
+        if(othello[7][0]) {
+            int mul = (othello[7][0] == player)? 1 : -1;
             for(int i = 6; i > 0; i--) {
-                if(othello[x][y] == othello[i][y]) val += 50;
+                if(othello[7][0] == othello[i][0]) val += mul * edge;
                 else break;
             }
             for(int j = 1; j < 7; j++) {
-                if(othello[x][y] == othello[x][j]) val += 50;
+                if(othello[7][0] == othello[7][j]) val += mul * edge;
                 else break;
             }
         }
-        else if(x == 7 && y == 7) {
+        if(othello[7][7]) {
+            int mul = (othello[7][7] == player)? 1 : -1;
             for(int i = 6; i > 0; i--) {
-                if(othello[x][y] == othello[i][y]) val += 50;
+                if(othello[7][7] == othello[i][7]) val += mul * edge;
                 else break;
             }
             for(int j = 6; j > 0; j--) {
-                if(othello[x][y] == othello[x][j]) val += 50;
+                if(othello[7][7] == othello[7][j]) val += mul * edge;
                 else break;
             }
         }
+        if((othello[0][1] == othello[0][2] == othello[0][3] ==
+            othello[0][4] == othello[0][5] == othello[0][6] == player)
+           && (othello[0][0] && othello[0][7]))
+           value += edge * 6;
+        if((othello[0][1] == othello[0][2] == othello[0][3] ==
+            othello[0][4] == othello[0][5] == othello[0][6] == (3 - player))
+           && (othello[0][0] && othello[0][7]))
+           value -= edge * 6;
+        if((othello[1][0] == othello[2][0] == othello[3][0] ==
+            othello[4][0] == othello[5][0] == othello[6][0] == player)
+           && (othello[0][0] && othello[7][0]))
+           value += edge * 6;
+        if((othello[1][0] == othello[2][0] == othello[3][0] ==
+            othello[4][0] == othello[5][0] == othello[6][0] == (3 - player))
+           && (othello[0][0] && othello[7][0]))
+           value -= edge * 6;
+        if((othello[7][1] == othello[7][2] == othello[7][3] ==
+            othello[7][4] == othello[7][5] == othello[7][6] == player)
+           && (othello[7][0] && othello[7][7]))
+           value += edge * 6;
+        if((othello[7][1] == othello[7][2] == othello[7][3] ==
+            othello[7][4] == othello[7][5] == othello[7][6] == (3 - player))
+           && (othello[7][0] && othello[7][7]))
+           value -= edge * 6;
+        if((othello[1][7] == othello[2][7] == othello[3][7] ==
+            othello[4][7] == othello[5][7] == othello[6][7] == player)
+           && (othello[0][7] && othello[7][7]))
+           value += edge * 6;
+        if((othello[1][7] == othello[2][7] == othello[3][7] ==
+            othello[4][7] == othello[5][7] == othello[6][7] == (3 - player))
+           && (othello[0][7] && othello[7][7]))
+           value -= edge * 6;
+        return val;
+    }
+    int weight() {
+        const int weight_table[SIZE][SIZE] =
+        {
+            { 500, -100, 20, 15, 15, 20, -100,  500},
+            {-100, -200, 10, 3,  3,  10, -200, -100},
+            { 20,   10,  15, 10, 10, 15,  10,   20},
+            { 15,   3,   10, 3,  3,  10,  3,    15},
+            { 15,   3,   10, 3,  3,  10,  3,    15},
+            { 20,   10,  15, 10, 10, 15,  10,   20},
+            {-100, -200, 10, 3,  3,  10, -200, -100},
+            { 500, -100, 20, 15, 15, 20, -100,  500},
+        };
+        int val = 0;
+        for(int i = 0; i < SIZE; i++)
+            for(int j = 0; j < SIZE; j++) {
+                if(othello[i][j] == player)
+                    val += weight_table[i][j];
+                else if(othello[i][j] == (3 - player))
+                    val -= weight_table[i][j];
+            }
         return val;
     }
     void setValue() {
@@ -109,34 +168,17 @@ public:
         int me = 0, op = 0;
         for(int i = 0; i < SIZE; i++)
             for(int j = 0; j < SIZE; j++) {
-                if(!othello[i][j]) // empty
-                    continue;
-                int mul = (othello[i][j] == player)? 1 : -1;
                 if(othello[i][j] == player) me++;
-                else op++;
-                if((i == 0 && j == 0) || (i == 0 && j == 7) ||
-                   (i == 7 && j == 0) || (i == 7 && j == 7)) {
-                    value += mul * corner(i, j);
-                }
-                else if((i == 1 && j == 1) || (i == 1 && j == 6) ||
-                        (i == 6 && j == 1) || (i == 6 && j == 6)) {
-                    value -= mul * 60;
-                }
-                else if((i == 0 && j == 1) || (i == 1 && j == 0) ||
-                        (i == 0 && j == 6) || (i == 1 && j == 7) ||
-                        (i == 6 && j == 0) || (i == 7 && j == 1) ||
-                        (i == 6 && j == 7) || (i == 7 && j == 6)) {
-                    value -= mul * 40;
-                }
-                /*else if(i == 0 || i == 7 || j == 0 || j == 7) {
-                    value += mul * 20;
-                }*/
+                else if(othello[i][j] == (3 - player)) op++;
             }
+        /* stability */
+        value += stability();
+        /* weight */
+        value += weight();
+        /* mobility (high to low) */
         int mul = (cur_player == player)? 1 : -1;
-        // difference
-        //value += (me - op) * 4 * (me + op) / ALL;
-        // mobility
         value += mul * valid_spots.size() * 2 * (1 - (me + op) / ALL);
+        /* difference (end) */
         if(game_end)
             value = me - op;
     }
@@ -146,9 +188,16 @@ public:
         cur_player = 3 - cur_player;
         change_valid_spots();
 
-        if(!valid_spots.size() && !game_end) {
+        if(!valid_spots.size()) {
+            std::vector<Point> tmp_valid_spots = valid_spots;
             cur_player = 3 - cur_player;
             change_valid_spots();
+            if(!valid_spots.size())
+                game_end = true;
+            else {
+                cur_player = 3 - cur_player;
+                valid_spots = tmp_valid_spots;
+            }
         }
         level++;
     }
@@ -189,18 +238,54 @@ private:
     }
     void change_valid_spots() {
         valid_spots.clear();
-        game_end = true;
         for(int i = 0; i < SIZE; i++)
             for(int j = 0; j < SIZE; j++) {
                 Point p(i, j);
-                if(!othello[i][j]) {
-                    game_end = false;
-                    if(is_valid_spot(p))
-                        valid_spots.push_back(p);
-                }
+                if(!othello[i][j] && is_valid_spot(p))
+                    valid_spots.push_back(p);
             }
     }
 };
+
+std::map<int, Point> val_move;
+int minimax(State s, int depth, int alpha, int beta) {
+    if(depth == 0 || s.game_end) {
+        s.setValue();
+        return s.value;
+    }
+    else if(s.cur_player == player) {
+        int val = VALUE_MIN;
+        int n = s.valid_spots.size();
+        for(int i = 0; i < n; i++) {
+            State next = s;
+            next.update(s.valid_spots[i]);
+            int m = minimax(next, depth - 1, alpha, beta);
+            val = std::max(val, m);
+            alpha = std::max(alpha, val);
+            if(s.level == 0)
+                val_move[m] = next_valid_spots[i];
+            if(beta <= alpha)
+                break;
+        }
+        return val;
+    }
+    else if(s.cur_player == (3 - player)) {
+        int val = VALUE_MAX;
+        int n = s.valid_spots.size();
+        for(int i = 0; i < n; i++) {
+            State next = s;
+            next.update(s.valid_spots[i]);
+            int m = minimax(next, depth - 1, alpha, beta);
+            val = std::min(val, m);
+            beta = std::min(beta, val);
+            if(s.level == 0)
+                val_move[m] = next_valid_spots[i];
+            if(beta <= alpha)
+                break;
+        }
+        return val;
+    }
+}
 
 void read_board(std::ifstream& fin) {
     fin >> player;
@@ -219,71 +304,21 @@ void read_valid_spots(std::ifstream& fin) {
     }
 }
 
-std::map<int, Point> val_move;
-
-int minimax(State s, int depth, int alpha, int beta) {
-    if(depth == 0 || s.game_end) {
-        s.setValue();
-        return s.value;
-    }
-    else if(s.cur_player == player) {
-        int val = VALUE_MIN;
-
-        int n = s.valid_spots.size();
-        for(int i = 0; i < n; i++) {
-            State next = s;
-            next.update(next_valid_spots[i]);
-            int m = minimax(next, depth - 1, alpha, beta);
-            val = std::max(val, m);
-            alpha = std::max(alpha, val);
-            if(s.level == 0)
-                val_move[m] = next_valid_spots[i];
-            if(beta <= alpha)
-                break;
-        }
-        return val;
-    }
-    else if(s.cur_player == (3 - player)) {
-        int val = VALUE_MAX;
-
-        int n = s.valid_spots.size();
-        for(int i = 0; i < n; i++) {
-            State next = s;
-            next.update(next_valid_spots[i]);
-            int m = minimax(next, depth - 1, alpha, beta);
-            val = std::min(val, m);
-            beta = std::min(beta, val);
-            if(s.level == 0)
-                val_move[m] = next_valid_spots[i];
-            if(beta <= alpha)
-                break;
-        }
-        return val;
-    }
-}
-
 void write_valid_spot(std::ofstream& fout) {
-    // Remember to flush the output to ensure the last action is written to file.
-    //fout << next_valid_spots[0].x << " " << next_valid_spots[0].y << std::endl;
-    //fout.flush();
-
-    std::vector<State> old_level, new_level;
-
     State s(board);
     s.valid_spots = next_valid_spots;
 
     int val = minimax(s, 1, VALUE_MIN, VALUE_MAX);
     Point best = val_move[val];
-    // Remember to flush the output to ensure the last action is written to file.
+    /* fout */
     fout << best.x << " " << best.y << std::endl;
     fout.flush();
 
-    val = minimax(s, 7, VALUE_MIN, VALUE_MAX);
+    val = minimax(s, 6, VALUE_MIN, VALUE_MAX);
     best = val_move[val];
-    // Remember to flush the output to ensure the last action is written to file.
+    /* fout */
     fout << best.x << " " << best.y << std::endl;
     fout.flush();
-
 }
 
 int main(int, char** argv) {
